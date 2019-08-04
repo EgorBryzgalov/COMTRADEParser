df801ab92b50321acebf550cd2a9d1b2c3c31d9d
Егор Брызгалов <egor@rus37.ru>
2019-08-04 12:36:07 +0300

    работа над парсером конфига. Добавлен класс канала.

---
+++ COMTRADEParser/Channel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMTRADEParser
+{
+    class Channel
+    {
+        public int Number { get; set; }
+
+        public string Id { get; set; }
+
+
+    }
+    class AnalogChannel : Channel
+    {
+        public AnalogChannel() : base()
+        {
+
+        }
+
+        public string Phase { get; set; }
+
+        public string Circuit { get; set; }
+
+        public string Unit { get; set; }
+
+        public float A { get; set; }
+
+        public float B { get; set; }
+
+        public float Skew { get; set; }
+
+        public int Min { get; set; }
+
+        public int Max { get; set; }
+
+        public int Primary { get; set; }
+
+        public int Secondary { get; set; }
+
+        public string PS { get; set; }
+    }
+    class DigitalChannel : Channel
+    {
+       public int DefaultState { get; set; }
+    }
+}
--- COMTRADEParser/ConfigReader.cs
+++ COMTRADEParser/ConfigReader.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Globalization;
 
 namespace COMTRADEParser
 {
@@ -14,6 +15,14 @@ namespace COMTRADEParser
         public int NumberOfAnalogs { get; set; }
 
         public int NumberOfDigitals { get; set; }
+
+        public List<AnalogChannel> AnalogChannels { get; set; } = new List<AnalogChannel>();
+
+        public List<DigitalChannel> DigitalChannels { get; set; } = new List<DigitalChannel>();
+
+        public int GridFrequency { get; set; }
+
+        public int SampleRate { get; set; }
 
         public ConfigReader()
         {
@@ -32,10 +41,11 @@ namespace COMTRADEParser
             {
                 lines.Add(line);
             }
+            int LineNum = 1;
 
             //Кол-во каналов, кол-во аналоговых каналов, кол-во цифровых каналов
             #region парсинг второй строки
-            string[] second = lines[1].Split(',');
+            string[] second = lines[LineNum].Split(',');
             NumberOfChannels = int.Parse(second[0]);
             StringBuilder StrBuilder = new StringBuilder();
             foreach (char c in second[1])
@@ -51,7 +61,64 @@ namespace COMTRADEParser
                     StrBuilder.Append(c);
             }
             NumberOfDigitals = int.Parse(StrBuilder.ToString());
+            LineNum++;
             #endregion
+
+            //Парсинг ирнформации о каналах
+            #region парсинг каналов
+            AnalogChannel GetAnalog(string[] s)
+            {
+                if (s.Length != 13)
+                    throw new ArgumentException("Ошибка чтения строки, неверное количество данных по аналогу");
+                AnalogChannel analog = new AnalogChannel();
+                analog.Number = int.Parse(s[0]);
+                analog.Id = s[1];
+                analog.Phase = s[2];
+                analog.Circuit = s[3];
+                analog.Unit = s[4];
+                analog.A = float.Parse(s[5].Replace('.',','));
+                analog.B = float.Parse(s[6].Replace('.', ','));
+                analog.Skew = float.Parse(s[7].Replace('.', ','));
+                analog.Min = int.Parse(s[8]);
+                analog.Max = int.Parse(s[9]);
+                analog.Primary = int.Parse(s[10]);
+                analog.Secondary = int.Parse(s[11]);
+                analog.PS = s[12];
+                return analog;
+                
+                
+            }
+            DigitalChannel GetDigital(string[] s)
+            {
+                if (s.Length != 3)
+                    throw new ArgumentException("Ошибка чтения строки, неверное количество данных по дискрету");
+                DigitalChannel digital = new DigitalChannel();
+                digital.Number = int.Parse(s[0]);
+                digital.Id = s[1];
+                digital.DefaultState = int.Parse(s[2]);
+                return digital;
+            }
+            for (int i = 1; i<=NumberOfChannels; i++)
+            {
+                string[] chan = lines[LineNum].Split(',');
+                if (chan.Length == 3)
+                    DigitalChannels.Add(GetDigital(chan));
+                else
+                    AnalogChannels.Add(GetAnalog(chan));
+                LineNum++;
+            }
+            
+            #endregion
+
+            GridFrequency = int.Parse(lines[LineNum]);
+            LineNum++;
+
+            if (int.Parse(lines[LineNum++]) != 1)
+                throw new NotImplementedException("Не реализована работа с несколькими частотами дискретизации");
+
+            string[] SmplRate = lines[LineNum++].Split(',');
+            SampleRate = int.Parse(SmplRate[0]);
+            
 
 
         }
